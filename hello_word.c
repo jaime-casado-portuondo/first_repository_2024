045fcd06b4f566f0d2d14ecfacac765651054aa5
jaime casado portuondo <9508192@alumnos.ufv.es>
2025-11-30 12:55:16 +0100

    Remove incorrect hello_world.c file

---
+++ hello_word.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main(){
+   // Print "Hello, World!" to the console
+   printf("Hello, World!\n")
+cat > hello_word.c << 'EOF'
+
+#include <stdio.h>
+
+int main(){
+   // Print "Hello, World!" to the console
+   printf("Hello, World!\n");
+
+ return 0; //Indicate succesful execution
+}
